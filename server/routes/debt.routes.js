6d819b9a82de79fc6708eaac14534d097c40e937
JKeiyuru <josephkeiyuru1@gmail.com>
2025-11-30 15:18:01 +0300

    Update debt.routes.js

--- server/routes/debt.routes.js
+++ server/routes/debt.routes.js
@@ -1,10 +1,11 @@
-// server/routes/debt.routes.js
+// server/routes/debt.routes.js - UPDATED
 
 import express from 'express';
 import {
   getAllDebts,
   recordDebtPayment,
-  generateDebtReport
+  generateDebtReport,
+  getTodayDebtPayments // NEW
 } from '../controllers/debt.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 
@@ -13,5 +14,6 @@ const router = express.Router();
 router.get('/', protect, getAllDebts);
 router.post('/payment', protect, recordDebtPayment);
 router.get('/report', protect, generateDebtReport);
+router.get('/payments/today', protect, getTodayDebtPayments); // NEW
 
 export default router;
