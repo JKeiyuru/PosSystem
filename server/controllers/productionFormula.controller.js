61b7abe74f4fcde15ed056a525a5cb1eb5a7a621
JKeiyuru <josephkeiyuru1@gmail.com>
2026-02-10 20:40:57 +0300

    Production Formula Addition

--- server/controllers/productionFormula.controller.js
+++ server/controllers/productionFormula.controller.js
@@ -1,13 +1,22 @@
 // server/controllers/productionFormula.controller.js - UPDATED with ingredient addition endpoint
 
-const ProductionFormula = require('../models/ProductionFormula');
-const Product = require('../models/Product');
-const Production = require('../models/Production');
-const Sale = require('../models/Sale');
+import ProductionFormula from '../models/ProductionFormula.js';
+import Product from '../models/Product.js';
+import Production from '../models/Production.js';
+import Sale from '../models/Sale.js';
 
-exports.createFormula = async (req, res) => {
+export const createFormula = async (req, res) => {
   try {
-    const { name, type, ingredients, finalProduct, customerName, customOutputName, defaultOutputBags, defaultOutputKgs } = req.body;
+    const {
+      name,
+      type,
+      ingredients,
+      finalProduct,
+      customerName,
+      customOutputName,
+      defaultOutputBags,
+      defaultOutputKgs
+    } = req.body;
 
     if (!name || !type || !ingredients || ingredients.length === 0) {
       return res.status(400).json({
@@ -68,7 +77,7 @@ exports.createFormula = async (req, res) => {
   }
 };
 
-exports.getAllFormulas = async (req, res) => {
+export const getAllFormulas = async (req, res) => {
   try {
     const formulas = await ProductionFormula.find()
       .populate('finalProduct', 'name')
@@ -88,7 +97,7 @@ exports.getAllFormulas = async (req, res) => {
   }
 };
 
-exports.getFormulaById = async (req, res) => {
+export const getFormulaById = async (req, res) => {
   try {
     const formula = await ProductionFormula.findById(req.params.id)
       .populate('finalProduct')
@@ -115,7 +124,7 @@ exports.getFormulaById = async (req, res) => {
 };
 
 // NEW: Update formula (for adding ingredients)
-exports.updateFormula = async (req, res) => {
+export const updateFormula = async (req, res) => {
   try {
     const { ingredients } = req.body;
     const formula = await ProductionFormula.findById(req.params.id);
@@ -127,7 +136,6 @@ exports.updateFormula = async (req, res) => {
       });
     }
 
-    // Enrich new ingredients with product details
     if (ingredients) {
       const enrichedIngredients = await Promise.all(
         ingredients.map(async (ing) => {
@@ -167,7 +175,7 @@ exports.updateFormula = async (req, res) => {
   }
 };
 
-exports.executeFormula = async (req, res) => {
+export const executeFormula = async (req, res) => {
   try {
     const formula = await ProductionFormula.findById(req.params.id)
       .populate('finalProduct')
@@ -180,8 +188,8 @@ exports.executeFormula = async (req, res) => {
       });
     }
 
-    const { 
-      scale = 'full', 
+    const {
+      scale = 'full',
       scaledIngredients,
       outputBags,
       outputKgs,
@@ -192,12 +200,15 @@ exports.executeFormula = async (req, res) => {
       substitutionDetails
     } = req.body;
 
-    const scaleMultiplier = scale === 'full' ? 1 : scale === 'half' ? 0.5 : 0.25;
+    const scaleMultiplier =
+      scale === 'full' ? 1 : scale === 'half' ? 0.5 : 0.25;
 
-    const ingredientsToUse = scaledIngredients || formula.ingredients.map(ing => ({
-      ...ing,
-      quantity: ing.quantity * scaleMultiplier
-    }));
+    const ingredientsToUse =
+      scaledIngredients ||
+      formula.ingredients.map((ing) => ({
+        ...ing,
+        quantity: ing.quantity * scaleMultiplier
+      }));
 
     for (const ing of ingredientsToUse) {
       const product = await Product.findById(ing.product);
@@ -210,7 +221,9 @@ exports.executeFormula = async (req, res) => {
 
       let quantityToDeduct = ing.quantity;
       if (ing.unit !== product.baseUnit) {
-        const subUnit = product.subUnits.find(su => su.name === ing.unit);
+        const subUnit = product.subUnits.find(
+          (su) => su.name === ing.unit
+        );
         if (subUnit) {
           quantityToDeduct = ing.quantity / subUnit.conversionRate;
         }
@@ -225,17 +238,23 @@ exports.executeFormula = async (req, res) => {
     }
 
     const totalCost = ingredientsToUse.reduce((sum, ing) => {
-      const product = formula.ingredients.find(i => i.product.toString() === ing.product.toString());
-      const price = ing.useBuyingPrice ? (product?.buyingPrice || 0) : (product?.sellingPrice || 0);
-      return sum + (price * ing.quantity);
+      const product = formula.ingredients.find(
+        (i) => i.product.toString() === ing.product.toString()
+      );
+      const price = ing.useBuyingPrice
+        ? product?.buyingPrice || 0
+        : product?.sellingPrice || 0;
+      return sum + price * ing.quantity;
     }, 0);
 
     for (const ing of ingredientsToUse) {
       const product = await Product.findById(ing.product);
       let quantityToDeduct = ing.quantity;
-      
+
       if (ing.unit !== product.baseUnit) {
-        const subUnit = product.subUnits.find(su => su.name === ing.unit);
+        const subUnit = product.subUnits.find(
+          (su) => su.name === ing.unit
+        );
         if (subUnit) {
           quantityToDeduct = ing.quantity / subUnit.conversionRate;
         }
@@ -247,14 +266,18 @@ exports.executeFormula = async (req, res) => {
 
     const productionData = {
       type: formula.type,
-      ingredients: ingredientsToUse.map(ing => {
-        const originalIng = formula.ingredients.find(i => i.product.toString() === ing.product.toString());
+      ingredients: ingredientsToUse.map((ing) => {
+        const originalIng = formula.ingredients.find(
+          (i) => i.product.toString() === ing.product.toString()
+        );
         return {
           product: ing.product,
           productName: ing.productName || originalIng?.productName,
           quantity: ing.quantity,
           unit: ing.unit,
-          unitCost: ing.useBuyingPrice ? (originalIng?.buyingPrice || 0) : (originalIng?.sellingPrice || 0),
+          unitCost: ing.useBuyingPrice
+            ? originalIng?.buyingPrice || 0
+            : originalIng?.sellingPrice || 0,
           usedBuyingPrice: ing.useBuyingPrice || false
         };
       }),
@@ -269,18 +292,30 @@ exports.executeFormula = async (req, res) => {
     };
 
     if (formula.type === 'standard') {
-      const finalOutputBags = outputBags !== undefined ? outputBags : (formula.defaultOutputBags || 0) * scaleMultiplier;
-      const finalOutputKgs = outputKgs !== undefined ? outputKgs : (formula.defaultOutputKgs || 0) * scaleMultiplier;
-      const outputQuantity = finalOutputBags + (finalOutputKgs / 50);
+      const finalOutputBags =
+        outputBags !== undefined
+          ? outputBags
+          : (formula.defaultOutputBags || 0) * scaleMultiplier;
+
+      const finalOutputKgs =
+        outputKgs !== undefined
+          ? outputKgs
+          : (formula.defaultOutputKgs || 0) * scaleMultiplier;
+
+      const outputQuantity =
+        finalOutputBags + finalOutputKgs / 50;
 
       productionData.finalProduct = formula.finalProduct._id;
       productionData.finalProductName = formula.finalProduct.name;
       productionData.outputQuantity = outputQuantity;
       productionData.outputBags = finalOutputBags;
       productionData.outputKgs = finalOutputKgs;
-      productionData.costPerUnit = outputQuantity > 0 ? totalCost / outputQuantity : 0;
+      productionData.costPerUnit =
+        outputQuantity > 0 ? totalCost / outputQuantity : 0;
 
-      const finalProduct = await Product.findById(formula.finalProduct._id);
+      const finalProduct = await Product.findById(
+        formula.finalProduct._id
+      );
       finalProduct.quantity += outputQuantity;
       await finalProduct.save();
     } else {
@@ -296,16 +331,16 @@ exports.executeFormula = async (req, res) => {
 
       if (sellImmediately && saleData) {
         const sale = await Sale.create({
-          customer: {
-            name: formula.customerName
-          },
-          items: [{
-            product: null,
-            name: formula.customOutputName,
-            quantity: 1,
-            unitPrice: sellingPrice,
-            total: sellingPrice
-          }],
+          customer: { name: formula.customerName },
+          items: [
+            {
+              product: null,
+              name: formula.customOutputName,
+              quantity: 1,
+              unitPrice: sellingPrice,
+              total: sellingPrice
+            }
+          ],
           subtotal: sellingPrice,
           total: sellingPrice,
           paymentMethod: saleData.paymentMethod || 'cash',
@@ -336,8 +371,7 @@ exports.executeFormula = async (req, res) => {
     };
 
     if (productionData.sale) {
-      const sale = await Sale.findById(productionData.sale);
-      response.sale = sale;
+      response.sale = await Sale.findById(productionData.sale);
     }
 
     res.status(201).json(response);
@@ -350,7 +384,7 @@ exports.executeFormula = async (req, res) => {
   }
 };
 
-exports.deleteFormula = async (req, res) => {
+export const deleteFormula = async (req, res) => {
   try {
     const formula = await ProductionFormula.findByIdAndDelete(req.params.id);
 
